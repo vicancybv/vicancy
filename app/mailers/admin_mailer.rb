fc6487d8ada5543119d8038d89aa852ce1a10aa7
Toby Marsden <toby@toby.org.uk>
2013-11-13 21:28:03 +0000

    Set correct recipient

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,24 +1,26 @@
 class AdminMailer < ActionMailer::Base
 
+  RECIPIENT = "loet@vicancy.com"
+
   helper :videos
 
   def message_email(name, email, message)
     @name = name
     @email = email
     @message = message
-    mail(to: "toby@toby.org.uk", subject: '[Vicancy] New enquiry', from: "#{name} <#{email}>")
+    mail(to: RECIPIENT, subject: '[Vicancy] New enquiry', from: "#{name} <#{email}>")
   end
 
   def edit_video_email(video, user_ip)
     @video = video
     @user_ip = user_ip
-    mail(to: "toby@toby.org.uk", subject: '[Vicancy] Video edits request', from: "Vicancy <toby@toby.org.uk>")
+    mail(to: RECIPIENT, subject: '[Vicancy] Video edits request', from: "Vicancy <#{RECIPIENT}>")
   end
 
   def delete_video_email(video, user_ip)
     @video = video
     @user_ip = user_ip
-    mail(to: "toby@toby.org.uk", subject: '[Vicancy] Delete video request', from: "Vicancy <toby@toby.org.uk>")
+    mail(to: RECIPIENT, subject: '[Vicancy] Delete video request', from: "Vicancy <#{RECIPIENT}>")
   end
 
 end
