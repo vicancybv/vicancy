223aa876d9890d6bda3ed6db1ab5dbcf5068d231
Toby Marsden <toby@toby.org.uk>
2013-11-13 21:53:43 +0000

    Devise translations

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -7,15 +7,15 @@ module VideosHelper
 
   def facebook_share_link(video)
     #"https://www.facebook.com/sharer/sharer.php?s=100&p[url]=#{video.video_url}&p[title]=#{video_title(video)}&p[summary]=#{I18n.t(:'sharing.facebook', locale: video.language, company: video.company, job_title: video.job_title, job_ad_url: video.job_ad_url, video_url: video.video_url)}"
-    "https://www.facebook.com/sharer/sharer.php?u=#{video.video_url}"
+    "https://www.facebook.com/sharer/sharer.php?u=#{u video.video_url}"
   end
 
   def twitter_share_link(video)
-    "https://twitter.com/intent/tweet?text=#{I18n.t(:'sharing.twitter', locale: video.language, company: video.company, job_title: video.job_title, job_ad_url: video.job_ad_url, video_url: video.video_url)}"
+    "https://twitter.com/intent/tweet?text=#{u I18n.t(:'sharing.twitter', locale: video.language, company: video.company, job_title: video.job_title, job_ad_url: video.job_ad_url, video_url: video.video_url)}"
   end
 
   def linkedin_share_link(video)
-    "http://www.linkedin.com/shareArticle?mini=true&url=#{video.youtube_url}&title=#{video_title(video)}&summary=#{I18n.t(:'sharing.linkedin', locale: video.language, company: video.company, job_title: video.job_title, job_ad_url: video.job_ad_url, video_url: video.video_url)}"
+    "http://www.linkedin.com/shareArticle?mini=true&url=#{u video.youtube_url}&title=#{u video_title(video)}&summary=#{u(!video.summary.blank? ? video.summary : I18n.t(:'sharing.linkedin', locale: video.language, company: video.company, job_title: video.job_title, job_ad_url: video.job_ad_url, video_url: video.video_url))}"
   end
 
   def embed_code(video)
