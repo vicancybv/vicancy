ea4b536e1b348518a0f0b63cd4e9dd129176f6f6
Toby Marsden <toby@toby.org.uk>
2013-11-13 09:48:09 +0000

    Default video order

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,4 +2,5 @@ class Video < ActiveRecord::Base
   belongs_to  :user
   attr_accessible :company, :job_ad_url, :job_title, :language, :summary, :title, :vimeo_id, :youtube_id, :user_id
   validates :language, presence: true
+  default_scope { order("created_at DESC") }
 end
