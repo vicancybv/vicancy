fbc06f5cee718a51ade7db46e39ebbfd1a12ada4
Pieter Moorman <pietermoorman@hotmail.com>
2013-12-12 06:35:45 +0100

    redirect to /mobile first attempt

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,8 +4,17 @@ class StaticPagesController < ApplicationController
 
   layout "static"
 
+  def mobile_device?
+    request.user_agent =~ /Mobile|webOS/
+  end
+  helper_method :mobile_device?
+
   def index
-    render layout: "static_index"
+    if mobile_device?
+      match '/' => redirect('/mobile') 
+    else
+      render layout: "static_index"
+    end
   end
 
   def en
