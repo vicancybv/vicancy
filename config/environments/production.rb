9a034096cda6f712b536bc733be09965074849f4
Pieter Moorman <pietermoorman@hotmail.com>
2014-01-28 16:45:33 +0100

    add mobile.css to precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,6 +49,8 @@ Vicancy::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( static_index.css )
   config.assets.precompile += %w( static_index.js )
+  config.assets.precompile += %w( mobile.css )
+  config.assets.precompile += %w( mobile_index.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
