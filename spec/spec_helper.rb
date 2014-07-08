f93cb3a52537035cde0611cb244d1e984e09bb86
Artem Fedorov <artemf@mail.ru>
2014-07-09 01:07:39 +0400

    GC.disable is no longer needed

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,3 @@
-GC.disable # fix bindings of caller segfaulting
-
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
