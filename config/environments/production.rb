5917bdf4f3f378d452df707dfec3d948f4324439
Artem Fedorov <artemf@mail.ru>
2014-08-22 16:01:23 +0200

    Try to fix asset compilation error when pushing to Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,5 @@
+require_relative Rails.root.join('app','models','settings')
+
 Vicancy::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
